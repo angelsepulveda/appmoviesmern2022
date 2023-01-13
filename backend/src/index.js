d3f0c60ecdad4b24161fff869ff741ff8e802883
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-01-13 15:13:54 -0300

    implementacion de rutas

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,24 +5,30 @@ import http from 'http'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
+import routes from './src/routes'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-const port =  process.env.PORT || 5000
+app.use('/api/v1', routes)
 
-const server = http.createServer(app);
+const port = process.env.PORT || 5000
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-	console.log('Mongodb connected')
-	server.listen(port, () => {
-		console.log(`Server listening on port ${port}`)
+const server = http.createServer(app)
+
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log('Mongodb connected')
+		server.listen(port, () => {
+			console.log(`Server listening on port ${port}`)
+		})
 	})
-}).catch(err => {
-	console.log({ err })
-	process.exit(1)
-})
+	.catch(err => {
+		console.log({ err })
+		process.exit(1)
+	})
