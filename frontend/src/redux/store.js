fda2f3f689598f732b570e8afe0bd365946f1200
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-01-17 15:26:23 -0300

    implementacion de slices

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import appSlice from './features/appSlice'
+import authModalSlice from './features/authModalSlice'
+import globalLoadingSlice from './features/globalLoadingSlice'
+import themeModeSlice from './features/themeModeSlice'
 import userSlice from './features/userSlice'
 
 const store = configureStore({
 	reducer: {
-		user: userSlice
+		user: userSlice,
+		themeMode: themeModeSlice,
+		appState: appSlice,
+		authModal: authModalSlice,
+		globalLoading: globalLoadingSlice
 	}
 })
 
