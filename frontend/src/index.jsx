f1e91fe1477e65bf1c3e917da00af1692be3c5dd
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-01-17 14:48:13 -0300

    implementacion de redux en frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import App from './App'
+import store from './redux/store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
